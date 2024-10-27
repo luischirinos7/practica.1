4845caf6bac0145dbdbca024fd71d63a0875f5d7
Luis Chirinos <luisemilio2006@gmail.com>
2024-10-27 10:11:18 -0400

    1 commit

---
+++ archivo_data.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+int main() {
+
+    ofstream archivo("Data.txt");
+
+    if (archivo.is_open()) {
+        archivo << "Contenido del archivo" << endl;
+        
+        archivo.close();
+
+        cout << "El archivo Data.txt se ha creado correctamente." << endl;
+    } else {
+        cout << "No se pudo crear el archivo Data.txt." << endl;
+    }
+
+    return 0;
+    }
