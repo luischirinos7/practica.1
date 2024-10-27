bab087c4395555ba39cf9be97362c3a78ff44b13
Luis Chirinos <luisemilio2006@gmail.com>
2024-10-27 10:37:23 -0400

    3 commit, agregado el mensaje final

--- archivo_data.cpp
+++ archivo_data.cpp
@@ -1,37 +1,40 @@
-#include <iostream>
-#include <fstream>
-#include <string>
+#include<iostream>
+#include<fstream>
+#include<string>
 
 using namespace std;
 
-int main() {
+int main(){
     string nombre, correo;
-    int cedula;
-    int N;
-
-    cout << "Ingrese la cantidad de registros: ";
-    cin >> N;
+    int cedula, N;
+    cout<<"Ingrese la cantidad de registros: ";
+    cin>>N;
+    cin.ignore();
 
     ofstream archivo("Data.txt");
-
-    if (archivo.is_open()) {
-        for (int i = 0; i < N; i++) {
-            cout << "Ingrese el nombre de la persona " << i + 1 << ": ";
-            cin >> nombre;
-
-            cout << "Ingrese la cédula de la persona " << i + 1 << ": ";
-            cin >> cedula;
-
-            cout << "Ingrese el correo de la persona " << i + 1 << ": ";
-            cin >> correo;
-
-            archivo << nombre << " " << cedula << " " << correo << endl;
+    if(archivo.is_open()){
+        for (int i=0; i<N; i++) {
+            cout<<"Ingrese el nombre de la persona "<<i+1<<": ";
+            cin>>nombre;
+            cout<<"Ingrese la cédula de la persona "<<i+1<<": ";
+            cin>>cedula;
+            cout<<"Ingrese el correo de la persona " <<i+1<<": ";
+            cin>>correo;
+            archivo<<nombre<<" "<<cedula<<" "<<correo<<endl;
         }
-
         archivo.close();
-        cout << "El archivo Data.txt se ha creado con " << N << " registros." << endl;
-    } else {
-        cout << "No se pudo crear el archivo Data.txt." << endl;
+    }else{
+        cout<<"No se pudo crear el archivo Data.txt."<<endl;
+        return 1;
+    }
+    ifstream leerArchivo("Data.txt");
+    if(leerArchivo.is_open()){
+        while(leerArchivo>>nombre>>cedula>>correo) {
+            cout<<"Mi nombre es "<<nombre <<", número de cédula: "<<cedula<<", mi correo es "<<correo<<endl;
+        }
+        leerArchivo.close();
+    }else{
+        cout<<"No se pudo abrir el archivo Data.txt para leer."<<endl;
     }
 
     return 0;
