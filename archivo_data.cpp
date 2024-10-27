bd350e9a2fec8381116287998577749ab48008fd
Luis Chirinos <luisemilio2006@gmail.com>
2024-10-27 10:27:09 -0400

    2 commit, registro agregado

--- archivo_data.cpp
+++ archivo_data.cpp
@@ -1,21 +1,38 @@
-#include<iostream>
-#include<fstream>
+#include <iostream>
+#include <fstream>
+#include <string>
 
 using namespace std;
 
-int main(){
+int main() {
+    string nombre, correo;
+    int cedula;
+    int N;
+
+    cout << "Ingrese la cantidad de registros: ";
+    cin >> N;
 
     ofstream archivo("Data.txt");
 
-    if(archivo.is_open()) {
-        archivo<<"Contenido del archivo"<<endl;
-        
-        archivo.close();
+    if (archivo.is_open()) {
+        for (int i = 0; i < N; i++) {
+            cout << "Ingrese el nombre de la persona " << i + 1 << ": ";
+            cin >> nombre;
 
-        cout<<"El archivo Data.txt se ha creado correctamente."<<endl;
+            cout << "Ingrese la cédula de la persona " << i + 1 << ": ";
+            cin >> cedula;
+
+            cout << "Ingrese el correo de la persona " << i + 1 << ": ";
+            cin >> correo;
+
+            archivo << nombre << " " << cedula << " " << correo << endl;
+        }
+
+        archivo.close();
+        cout << "El archivo Data.txt se ha creado con " << N << " registros." << endl;
     } else {
-        cout<<"No se pudo crear el archivo Data.txt."<<endl;
+        cout << "No se pudo crear el archivo Data.txt." << endl;
     }
 
     return 0;
-    }
+}
